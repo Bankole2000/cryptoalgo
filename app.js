3c70cbf2684274d11e1906c39e27d40f3a17bcdd
Bankole Esan <techybanky@gmail.com>
2019-09-17 18:24:14 +0100

    updated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //Initialize
 M.AutoInit();
+const textregex = /^[a-zA-Z\s]*$/;
 
 // GET UI Elements
 const rot13input = document.querySelector("#rot13");
@@ -9,7 +10,28 @@ const rot13output = document.querySelector('#rot13result');
 const alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', " "];
 
 // Declare functions
-const rot13encrypt = (text) => {
+const flagIfInvalid = (isValid, target) => {
+    isValid? markAsValid(target): markAsInvalid(target);
+};
+
+const markAsValid = (target) => {
+  target.classList.add('valid'); 
+  target.classList.remove('invalid');
+}
+
+const markAsInvalid = (target) => {
+  target.classList.remove('valid'); 
+  target.classList.add('invalid');
+}
+
+const unMark = (target) => {
+  target.classList.remove('valid');
+  target.classList.remove('invalid');
+}
+
+$(document).on('focusin',`#rot3input`,function(){unmark(this)});
+
+const rot13encrypt = (text, task) => {
   let output = "";
   let textArray = Array.from(text.toUpperCase());
   textArray.forEach((letter, textindex, array) => {
@@ -20,12 +42,15 @@ const rot13encrypt = (text) => {
     
   })
   rot13output.textContent = output;
+  M.toast({ html: `Message ${task}ed`, classes: 'success' });
 };
 rot13encbtn.addEventListener('click', ()=>{
-  M.toast({ html: `Message Encrypted` });
-  rot13encrypt(rot13input.value);
+  isValid = rot13input.value !== "" && textregex.test(rot13input.value) ? true: false;
+  flagIfInvalid(isValid, rot13input);
+  isValid ? rot13encrypt(rot13input.value, "Encrypt"): M.toast({ html: `Text is Invalid`, classes: "error" });
 });
 rot13decbtn.addEventListener('click', ()=>{
-  M.toast({ html: `Message Decrypted` });
-  rot13encrypt(rot13input.value);
+  isValid = rot13input.value !== "" && textregex.test(rot13input.value) ? true: false;
+  flagIfInvalid(isValid, rot13input);
+  isValid ? rot13encrypt(rot13input.value, "Decrypt"): M.toast({ html: `Text is Invalid`, classes: "error" });
 });
